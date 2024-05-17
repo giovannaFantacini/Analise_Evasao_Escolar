340caff9dfb0d8294f418080b816ff59be7d587c
Giovanna Fantacini <74154716+giovannaFantacini@users.noreply.github.com>
2024-05-17 14:42:18 -0300

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -2,6 +2,7 @@ import pandas as pd
 import streamlit as st
 @st.cache_data
 def carrega_dados():
-    df_path = 'RelatorioAlunos2024Clear.xlsx'
+    current_dir = os.path.dirname(__file__)
+    df_path = os.path.join(current_dir, 'RelatorioAlunos2024Clear.xlsx')
     df = pd.read_excel(df_path, header=0)
     return df
